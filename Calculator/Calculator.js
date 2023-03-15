41cd1c4cbe138b005994af360308e94290425559
pdbautista0 <patdi.bautista@gmail.com>
2023-03-15 23:49:04 +0800

    Edit calculator defintion

---
+++ Calculator/Calculator.js
@@ -0,0 +1,192 @@
+
+// class Calculator {
+
+//   OnClickButton = {
+//     OnClickButton1: '1',
+//     OnClickButton2: '2',
+//     OnClickButton3: '3',
+//     OnClickButton4: '4',
+//     OnClickButton5: '5',
+//     OnClickButton6: '6',
+//     OnClickButton7: '7',
+//     OnClickButton8: '8',
+//     OnClickButton9: '9',
+//     OnClickButton0: '0',
+//     OnClickButtonAdd: '+',
+//     OnClickButtonMinus: '-',
+//     OnClickButtonMult: '*',
+//     OnClickButtonDiv: '/',
+//     OnClickButtonC: 'C',
+//     OnClickButtonCE: 'CE',
+//     OnClickButtonPernt: '%',
+//     OnClickButtonEquals: '='
+//   }
+
+//   constructor(){
+//     this.params = {
+//       operand1: '',
+//       operand2: '',
+//       display: '',
+//     }
+
+//     this.operator = '';
+//     this.isOnOperatorEn = false;
+//   }
+
+//   OnClickButton(input) {
+//     switch(input){
+//       case  OnClickButton.OnClickButton0:
+//       this.updateOperand(this.params, OnClickButton.OnClickButton0);
+//       break;
+//       case OnClickButton.OnClickButton1:
+//       this.updateOperand(this.params, OnClickButton.OnClickButton1);
+//       break;
+//       case OnClickButton.OnClickButton2:
+//       this.updateOperand(this.params, OnClickButton.OnClickButton2);
+//       break;
+//       case OnClickButton.OnClickButton3:
+//       this.updateOperand(this.params, OnClickButton.OnClickButton3);
+//       break;
+//       case OnClickButton.OnClickButton4:
+//       this.updateOperand(this.params, OnClickButton.OnClickButton4);
+//       break;
+//       case OnClickButton.OnClickButton5:
+//       // TODO
+//       break;
+//       case OnClickButton.OnClickButton6:
+//       // TODO
+//       break;
+//       case OnClickButton.OnClickButton7:
+//       // TODO
+//       break;
+//       case OnClickButton.OnClickButton8:
+//       // TODO
+//       break;
+//       case OnClickButton.OnClickButton9:
+//       // TODO
+//       break;
+//       case OnClickButton.OnClickButtonAdd:
+//       this.updateOperator(OnClickButton.OnClickButtonAdd);
+//       break;
+//       case OnClickButton.OnClickButtonMinus:
+//       this.operator = OnClickButton.OnClickButtonMinus;
+//       break;
+//       case OnClickButton.OnClickButtonMult:
+//       this.operator = OnClickButton.OnClickButtonMult;
+//       break;
+//       case OnClickButton.OnClickButtonDiv:
+//       this.operator = OnClickButton.OnClickButtonDiv;
+//       break;
+//       case OnClickButton.OnClickButtonC:
+//       // TODO
+//       break;
+//       case OnClickButton.OnClickButtonCE:
+//       // TODO
+//       break;
+//       case OnClickButton.OnClickButtonPernt:
+//       // TODO
+//       break;
+//     }
+//   }
+
+//   updateOperand(operand, key) {
+
+//     let operandToOperate = ''
+
+//     if((this.params.operand == '') && (this.operator == '')){
+//       operandToOperate = 'operand1'
+//     }else if ((this.params.operand1 != '') && (this.operator != '') ) {
+//       operandToOperate = 'operand2'
+//     }else {
+//       operandToOperate = 'operand1'
+//     }
+
+//     console.log(operandToOperate)
+//     switch (operandToOperate) {
+//       case 'operand1':
+//       if(operand == ''){
+//         operand.operand1 = key;
+//         operand.display = operand.operand1
+//       }else {
+//         operand.operand1 = operand.operand1 + key;
+//       }
+//       operand.display = operand.operand1;
+//       break;
+//       case 'operand2':
+//       if(operand == ''){
+//         operand.operand2 = key;
+//       }else {
+//         operand.operand2 = operand.operand2 + key;
+//       }
+//       operand.display = operand.operand2;
+//       break;
+//       default:
+//     }
+
+//     console.log('display: ' + operand.operand1 + '' + this.operator + '' + operand.operand2)
+
+//   }
+
+//   updateOperator(key){
+//     if(this.params.operand1 != ''){
+//       this.operator = key;
+//     }
+//   }
+
+// }
+
+
+
+
+// console.log('Operand1')
+// const calculator = new Calculator();
+
+// document.getElementById('button0').onclick = function () {calculator.OnClickButton(OnClickButton.OnClickButton0)};
+// document.getElementById('buttonPlus').onclick = function () {calculator.OnClickButton(OnClickButton.OnClickButtonAdd)};
+
+
+/* *
+
+calculator.OnClickButton(OnClickButton.OnClickButton0);
+console.log('operand1 ' + calculator.params.operand1);
+calculator.OnClickButton(OnClickButton.OnClickButton0);
+console.log('operand1 ' + calculator.params.operand1);
+calculator.OnClickButton(OnClickButton.OnClickButton3);
+calculator.OnClickButton(OnClickButton.OnClickButton4);
+console.log('operand1 ' + calculator.params.operand1);
+calculator.OnClickButton(OnClickButton.OnClickButtonAdd);
+calculator.OnClickButton(OnClickButton.OnClickButton4);
+console.log('operand2 ' + calculator.params.operand2);
+
+*/
+
+let operand1, operand2, operation
+operand1= operand2 = ''
+operantion = null;
+
+function inputNumber(char){
+  if(char.length === 1){
+    operand1 = operand1 + char
+  }else{
+    alert("invalid input")
+  }
+}
+
+function inputOperation(x){
+  if(operand2 == ''){
+    operation = x;
+    operand2 = operand1.valueOf();
+    operand1 = ''
+  }else{
+    let answer = evaluate(operation, operand1, operand2);
+    console.log(answer);
+    operand1 = `${answer}`;
+    operand2 = '';
+  }
+}
+
+function evaluate(operation, operand1, operand2){
+  if(operation == 'add'){
+    return Number(operand1) + Number(operand2)
+  }
+}
---
+++ Calculator/CalculatorEvent.js
@@ -0,0 +1,67 @@
+class OnClickButton1 {
+  constructor (){}
+}
+
+class OnClickButton2 {
+  constructor (){}
+}
+
+class OnClickButton3 {
+  constructor (){}
+}
+
+class OnClickButton4 {
+  constructor (){}
+}
+
+class OnClickButton5 {
+  constructor (){}
+}
+
+class OnClickButton6 {
+  constructor (){}
+}
+
+class OnClickButton7 {
+  constructor (){}
+}
+
+class OnClickButton8 {
+  constructor (){}
+}
+
+class OnClickButton9 {
+  constructor (){}
+}
+
+class OnClickButtonEqual {
+  constructor (){}
+}
+
+class OnClickButtonPlus {
+  constructor (){}
+}
+
+class OnClickButtonMinus {
+  constructor (){}
+}
+
+class OnClickButtonMult {
+  constructor (){}
+}
+
+class OnClickButtonDiv {
+  constructor (){}
+}
+
+class OnClickButtonC {
+  constructor (){}
+}
+
+class OnClickButtonCE {
+  constructor (){}
+}
+
+class OnClickButtonPercent {
+  constructor (){}
+}
